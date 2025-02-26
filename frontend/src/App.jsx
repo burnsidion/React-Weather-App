8c6f32934a0d1bcfa539ce6ba98590a8ebc83458
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2025-02-26 15:14:02 -0700

    Setting up react router and HomePage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import "./index.css";
 import CityCard from "./components/CityCard";
+import HomePage from "./pages/HomePage";
 function App() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <CityCard city="Boulder" temperature={72} weather="Sunny" />
+      {/* <CityCard city="Boulder" temperature={72} weather="Sunny" /> */}
+      <HomePage />
     </div>
   );
 }
