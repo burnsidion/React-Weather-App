69ce2c291e6a18ed45179f09952b11d181221da2
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2025-02-26 22:36:18 -0700

    Refactored CityCard with destructuring and optional chaining

--- frontend/src/components/CityCard.jsx
+++ frontend/src/components/CityCard.jsx
@@ -1,27 +1,27 @@
 import PropTypes from "prop-types";
 
 const CityCard = ({ city }) => {
+  const { max = 0, min = 0 } = city.weather?.daily?.[0]?.temp ?? {};
+
   return (
     <div
       className="flex py-6 px-3 bg-weather-secondary rounded-md shadow-md cursor-pointer border-4 border-weather-primary hover:animate-pulse sm:animate-none"
       tabIndex="0"
     >
       <div className="flex flex-col flex-1">
-        <h2 className="text-3xl">{city.city}</h2>
-        <h3>{city.state}</h3>
+        <h2 className="text-3xl"> {city.city} </h2>
+        <h3> {city.state}</h3>
       </div>
       <div className="flex flex-col gap-2">
         <p className="text-3xl self-end">
-          {rounder(city.weather.current.temp)}°
+          {rounder(city.weather?.current?.temp ?? 0)}°
         </p>
         <div className="flex gap-2 self-end">
-          <span className="text-xs self-end">
-            H: {rounder(city.weather.daily[0].temp.max)}°
-          </span>
-          <span className="text-xs">L: {city.weather.daily[0].temp.min}</span>
+          <span className="text-xs self-end">H: {rounder(max)}°</span>
+          <span className="text-xs">L: {rounder(min)}°</span>
         </div>
-        <span className="text-xs">
-          L: {rounder(city.weather.daily[0].temp.min)}°
+        <span className="capitalize self-end">
+          {city.weather.current.weather[0].description}
         </span>
       </div>
     </div>
